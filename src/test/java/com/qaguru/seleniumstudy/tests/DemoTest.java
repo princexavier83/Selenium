42a8ab3933d25be0b00983c03956f90de0cb5167
princexavier83 <princexavier83@gmail.com>
2020-10-03 20:25:05 -0400

    Add test for searching a product  closes #2

--- src/test/java/com/qaguru/seleniumstudy/tests/DemoTest.java
+++ src/test/java/com/qaguru/seleniumstudy/tests/DemoTest.java
@@ -1,9 +1,11 @@
 package com.qaguru.seleniumstudy.tests;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DemoTest {
@@ -17,9 +19,24 @@ public class DemoTest {
        driver.get(baseUrl);
        String expTitle = "Furniture, Kitchen, Dining Room, Entertainment, Bedroom Sets, Outdoor, Fireplaces";
        String actTitle = driver.getTitle();
-       Assert.assertEquals(actTitle,expTitle);
+       Assert.assertEquals(actTitle, expTitle);
 
        Thread.sleep(1000);
        driver.quit();
+   }
+       @Test
+
+       public void testSearchForProduct() {
+           WebDriverManager.chromedriver().setup();
+           WebDriver driver = new ChromeDriver();
+           driver.get(baseUrl);
+           WebElement txtSearch =driver.findElement(By.name("search_field"));
+           txtSearch.sendKeys("table");
+
+           WebElement btnSearch = driver.findElement(By.xpath("//*[@id=\"search\"]/form/div/span/input"));
+           btnSearch.click();
+           WebElement linkResult = driver.findElement(By.linkText("Atlantic Height Adjustable Standing Desk Converter Large"));
+
+           driver.quit();
     }
 }
